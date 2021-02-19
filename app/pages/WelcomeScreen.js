1546ca7a84ff6188f30ff7ca02d6abe0f6eaeee6
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-19 21:35:29 +0530

    Learnt ActivityIndicator

--- app/pages/WelcomeScreen.js
+++ app/pages/WelcomeScreen.js
@@ -2,7 +2,14 @@ import React from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 const WelcomeScreen = () => {
-  return <ActivityIndicator />;
+  return (
+    <View>
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" color="#0000ff" />
+      <ActivityIndicator size="large" color="#00ff00" />
+    </View>
+  );
 };
 
 export default WelcomeScreen;
