a172efdd13e0c1dc2cd74bf9245486199e5db16b
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-20 21:54:48 +0530

    Styling of Text

--- app/pages/WelcomeScreen.js
+++ app/pages/WelcomeScreen.js
@@ -1,25 +1,38 @@
 import React from "react";
 import {
   StyleSheet,
-  Button,
   View,
   SafeAreaView,
   Text,
-  Alert,
-  Platform,
-  StatusBar,
-  ScrollView,
   TextInput,
+  TouchableOpacity,
+  Dimensions,
 } from "react-native";
 
 const WelcomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.mainHeading}>Signup</Text>
-        <TextInput underlineColorAndroid="transparent" />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Sign Up</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="Email"
+        placeholderTextColor="#9a73ef"
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="Password"
+        placeholderTextColor="#9a73ef"
+        autoCapitalize="none"
+      />
+
+      <TouchableOpacity style={styles.submitButton}>
+        <Text style={styles.submitButtonText}> Submit </Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -28,16 +41,31 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  mainText: {
+    color: "#7a42f4",
+    fontSize: 20,
   },
   input: {
+    padding: 10,
     margin: 15,
     height: 40,
     borderColor: "#7a42f4",
     borderWidth: 1,
+    width: Dimensions.get("window").width - 150,
   },
-  mainHeading: {
-    fontSize: 20,
+  submitButton: {
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40,
+    width: Dimensions.get("window").width - 150,
+    borderRadius: 100,
+  },
+  submitButtonText: {
+    color: "white",
+    textAlign: "center",
   },
 });
