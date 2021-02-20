da81cf804264087bd4cff849993ecaa5da960970
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-20 21:35:00 +0530

    TextInput done

--- app/pages/WelcomeScreen.js
+++ app/pages/WelcomeScreen.js
@@ -8,62 +8,16 @@ import {
   Alert,
   Platform,
   StatusBar,
+  ScrollView,
+  TextInput,
 } from "react-native";
-
-const Separator = () => <View style={styles.separator} />;
 
 const WelcomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text style={styles.title}>
-          The title and onPress handler are required. It is recommended to set
-          accessibilityLabel to help make your app usable by everyone.
-        </Text>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert("Simple Button pressed")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On
-          iOS, the color prop controls the color of the text. On Android, the
-          color adjusts the background color of the button.
-        </Text>
-        <Button
-          title="Press me"
-          color="#f194ff"
-          onPress={() => Alert.alert("Button with adjusted color pressed")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          All interaction for the component are disabled.
-        </Text>
-        <Button
-          title="Press me"
-          disabled
-          onPress={() => Alert.alert("Cannot press this one")}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          This layout strategy lets the title define the width of the button.
-        </Text>
-        <View style={styles.fixToText}>
-          <Button
-            title="Left button"
-            onPress={() => Alert.alert("Left button pressed")}
-          />
-          <Button
-            title="Right button"
-            onPress={() => Alert.alert("Right button pressed")}
-          />
-        </View>
+        <Text style={styles.mainHeading}>Signup</Text>
+        <TextInput underlineColorAndroid="transparent" />
       </View>
     </SafeAreaView>
   );
@@ -74,23 +28,16 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
-  title: {
-    textAlign: "center",
-    marginVertical: 8,
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
   },
-
-  fixToText: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  mainHeading: {
+    fontSize: 20,
   },
 });
