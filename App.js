2cb559a0ff7ac132b352fc02a84095266105b33e
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-24 22:02:46 +0530

    Navigation setup Completed

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React, { useState } from "react";
 import SignupScreen from "./app/pages/SignupScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Signin from "./app/pages/SigninScreen.js";
+import Signup from "./app/pages/SignupScreen.js";
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Signin" component={Signin} />
+    </Stack.Navigator>
+  );
+}
 
 const App = () => {
-  return <SignupScreen />;
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
 };
 
 export default App;
